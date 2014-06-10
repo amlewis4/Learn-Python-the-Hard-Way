dc553f05b054422b48fe4e845f9900f402b7a5d2
Ari Lewis <aml183@case.edu>
2014-06-10 15:04:42 -0400

    first commit

--- exl3.py
+++ exl3.py
@@ -1,35 +1,23 @@
-# This is a statement
 print 'I will now count my chickens:'
 
-# How Many Hens I have
-print 'Hens', 25 + 30 / 6
-# How Many Roosters I have
+print "Hens", 25 + 30 / 6
 print 'Roosters', 100 - 25 * 3 % 4
 
-# This is a statement
 print 'Now I will count the eggs:'
 
-# The math to get how many eggs I have
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 2 / 4 + 6
 
-# A question 
-print 'Is it true that 3 + 2 < 5 -7?'
+print 'is it true that 3 + 2 < 5 - 7'
 
-# False
 print 3 + 2 < 5 - 7
 
-# 5
-print 'What is 3 + 2?', 3 + 2
+print 'what is 3 + 2?', 3 + 2
+print 'what is 5 - 7?', 5 - 7
 
-# -2
-print 'What is 5 - 7', 5-7
-# A Statement
-print "Oh, that's why it's False."
-# I'll think about it
-print 'How about some more'
-#It is greater
-print 'Is it greater?', 5 < -2
-#Hey
-print 'Is it greater or equal', 5 >= -2
-# <3 
-print 'Is it less or equal?', 5 <= -2
+print "oh, that's why it's false."
+
+print 'how about some more.'
+
+print 'is it greater?', 5 > -2
+print 'is it greater or equal?', 5 >= -2
+print 'is it less or equal?', 5 <= -2
